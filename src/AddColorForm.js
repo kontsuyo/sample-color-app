12b5c0db39266988281259b85e4b15f332a66c67
Tsuyoshi Kondo <a9n1105@gmail.com>
2023-12-03 10:29:28 +0900

    重複したコードの抽象化

--- src/AddColorForm.js
+++ src/AddColorForm.js
@@ -1,31 +1,25 @@
-import { useState } from "react";
+import { useInput } from "./hook";
 
 export default function AddColorForm({ onNewColor = (f) => f }) {
-  const [title, setTitle] = useState(""); // userの入力値にステート経由でアクセス
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
-  const submit = (e) => {
-    e.preventDefault();
-    onNewColor(title, color);
-    setTitle("");
-    setColor("");
+  const submit = (event) => {
+    event.preventDefault();
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle("");
+    resetColor("");
   };
 
   return (
     <form onSubmit={submit}>
       <input
-        value={title}
-        onChange={(event) => setTitle(event.target.value)} //targetフィールドはDOMへの参照。event文字を入力するたびに関数を呼び出し、再描画される仕組み
+        {...titleProps}
         type="text"
         placeholder="color title..."
         required //必須入力欄
       />
-      <input
-        value={color}
-        onChange={(event) => setColor(event.target.value)}
-        type="color"
-        required
-      />
+      <input {...colorProps} type="color" required />
     </form>
   );
 }
